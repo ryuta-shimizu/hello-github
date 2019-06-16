65416be584114017d0eadf08e5d5d98964b9917c
ryuta-shimizu <51716084+ryuta-shimizu@users.noreply.github.com>
2019-06-17 00:55:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,107 +1,53 @@
-# -*- coding: utf-8 -*-
-
-
-
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-
-#  not use this file except in compliance with the License. You may obtain
-
-#  a copy of the License at
-
-#
-
-#       https://www.apache.org/licenses/LICENSE-2.0
-
-#
-
-#  Unless required by applicable law or agreed to in writing, software
-
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-
-#  License for the specific language governing permissions and limitations
-
-#  under the License.
-
-
-
+from flask import Flask, request, abort
 import os
 
-import sys
-
-from argparse import ArgumentParser
-
-
-
-from flask import Flask, request, abort
-
 from linebot import (
-
     LineBotApi, WebhookHandler
-
 )
-
 from linebot.exceptions import (
-
     InvalidSignatureError
-
 )
-
 from linebot.models import (
-
     MessageEvent, TextMessage, TextSendMessage,
-
 )
-
-
 
 app = Flask(__name__)
 
+#環境変数取得
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-# get channel_secret and channel_access_token from your environment variable
+@app.route("/")
+def hello_world():
+    return "hello world!"
 
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-if channel_secret is None:
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-
-    sys.exit(1)
-
-if channel_access_token is None:
-
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-
-    sys.exit(1)
-
-
-
-line_bot_api = LineBotApi(channel_access_token)
-
-handler = WebhookHandler(channel_secret)
+    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-
-def message_text(event):
-
+def handle_message(event):
     line_bot_api.reply_message(
-
         event.reply_token,
-
-        TextSendMessage(text=event.message.text)
-
-    )
-
-
-
-
+        TextSendMessage(text=event.message.text))
 
 if __name__ == "__main__":
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
+#    app.run()
+    port = int(os.getenv("PORT"))
     app.run(host="0.0.0.0", port=port)
